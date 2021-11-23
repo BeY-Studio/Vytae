34e356e2b081d4723b65ef452ceacb7bb9e378f7
ShaheryarAbid <shaheryar93@live.com>
2021-11-24 02:55:51 +0500

    Removed wow js library

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.css';
-import {useEffect} from "react";
+// import {useEffect} from "react";
 const isServer = typeof window === 'undefined';
-const WOW = !isServer ? require('wow.js') : null;
+// const WOW = !isServer ? require('wow.js') : null;
 
 function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-        new WOW().init();
-    }, []);
+    // useEffect(() => {
+    //     new WOW().init();
+    // }, []);
     
     return (
         <>
