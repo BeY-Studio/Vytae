1b19b157b8434994a98b1ecb935abd8d47d51732
ShaheryarAbid <shaheryar93@live.com>
2021-12-12 18:25:10 +0500

    Fixed the js errors

--- components/product-slider/bg.js
+++ components/product-slider/bg.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { gsap } from "gsap";
 
 const BgComponent = () => {
 
-    const waves = gsap.timeline({
-        repeat: -1,
-        defaults: {
-            duration: 4.0,
-            ease: 'none'
-        }
-    });
+    useEffect(() => {
+        const waves = gsap.timeline({
+            repeat: -1,
+            defaults: {
+                duration: 4.0,
+                ease: 'none'
+            }
+        });
+    
+        waves
+            .to('.c1', { scale: 2, opacity: 1 }, 0)
+            .to('.c2', { scale: 1.5, opacity: 0.5 }, 0)
+            .to('.c3', { scale: 1.33, opacity: 0 }, 0);
+    }, []);
 
-    waves
-        .to('.c1', { scale: 2, opacity: 1 }, 0)
-        .to('.c2', { scale: 1.5, opacity: 0.5 }, 0)
-        .to('.c3', { scale: 1.33, opacity: 0 }, 0);
 
-        
     return (
         <div className="inner">
             <div className="box">
 
                 <svg className="circle c1" width="458" height="458" viewBox="0 0 458 458" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g filter="url(#filter0_ddii)">
-                        <circle cx="229" cy="229" r="192" stroke="#EDEBEA" stroke-width="26" />
+                        <circle cx="229" cy="229" r="192" stroke="#EDEBEA" strokeWidth="26" />
                     </g>
                     <defs>
-                        <filter id="filter0_ddii" x="0" y="0" width="458" height="458" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <filter id="filter0_ddii" x="0" y="0" width="458" height="458" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
                             <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
                             <feOffset dx="-6" dy="-6" />
                             <feGaussianBlur stdDeviation="9" />
@@ -57,11 +59,11 @@ const BgComponent = () => {
 
                 <svg className="circle c2" width="458" height="458" viewBox="0 0 458 458" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g filter="url(#filter0_ddii)">
-                        <circle cx="229" cy="229" r="192" stroke="#EDEBEA" stroke-width="26" />
+                        <circle cx="229" cy="229" r="192" stroke="#EDEBEA" strokeWidth="26" />
                     </g>
                     <defs>
-                        <filter id="filter0_ddii" x="0" y="0" width="458" height="458" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <filter id="filter0_ddii" x="0" y="0" width="458" height="458" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
                             <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
                             <feOffset dx="-6" dy="-6" />
                             <feGaussianBlur stdDeviation="9" />
@@ -92,11 +94,11 @@ const BgComponent = () => {
 
                 <svg className="circle c3" width="458" height="458" viewBox="0 0 458 458" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g filter="url(#filter0_ddii)">
-                        <circle cx="229" cy="229" r="192" stroke="#EDEBEA" stroke-width="26" />
+                        <circle cx="229" cy="229" r="192" stroke="#EDEBEA" strokeWidth="26" />
                     </g>
                     <defs>
-                        <filter id="filter0_ddii" x="0" y="0" width="458" height="458" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <filter id="filter0_ddii" x="0" y="0" width="458" height="458" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
                             <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
                             <feOffset dx="-6" dy="-6" />
                             <feGaussianBlur stdDeviation="9" />
