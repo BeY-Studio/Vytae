daca4c3713a1ab9a824d2f19c94659c0e97e3034
aedjoel <aedjoel0x@gmail.com>
2022-01-06 10:05:53 +0100

    changes

--- components/related-products/index.js
+++ components/related-products/index.js
@@ -1,36 +1,41 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 
 const RelatedProducts = (props) => {
-    return (
-        <div className="related_products">
-            <h2 className="section_h2">{ props.lang === "it" ? "Prodotti correlati" : "Related Products" }</h2>
-            <div className="inner">
-                {
-                    props.products?.length > 0 ?
-                        props.products.map(product => (
-                            product.id !== props.productDetail.id ?
-                                <div className="product_con" key={product.id}>
-                                    <div className="img_con">
-                                        <img src={product?.images[0].src} alt={product?.title} />
-                                    </div>
-                                    <div className="title">{product?.title}</div>
-                                    <button className="buy">
-                                        <Link 
-                                            href={"/" + product.title.split(" ").join("-").replace("%", "%25") + "?lang=" + props.lang}
-                                            key={product.id}
-                                        >
-                                            <a>Buy now</a>
-                                        </Link>
-                                    </button>
-                                </div>
-                            : null
-                        ))
-                    : null
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="related_products">
+      <h2 className="section_h2">
+        {props.lang === "it" ? "Prodotti correlati" : "Related Products"}
+      </h2>
+      <div className="inner">
+        {props.products?.length > 0
+          ? props.products.map((product) =>
+              product.id !== props.productDetail.id ? (
+                <div className="product_con" key={product.id}>
+                  <div className="img_con">
+                    <img src={product?.images[0].src} alt={product?.title} />
+                  </div>
+                  <div className="title">{product?.title}</div>
+                  <button className="buy">
+                    <Link
+                      href={
+                        "/" +
+                        product.title.split(" ").join("-").replace("%", "%25") +
+                        "?lang=" +
+                        props.lang
+                      }
+                      key={product.id}
+                    >
+                      <a>{props.lang === "it" ? "Acquista ora" : "Buy Now"}</a>
+                    </Link>
+                  </button>
+                </div>
+              ) : null
+            )
+          : null}
+      </div>
+    </div>
+  );
 };
 
 export default RelatedProducts;
