ad7706841432fc8691738b278f0ef31370cae860
ShaheryarAbid <shaheryar93@live.com>
2022-04-01 23:41:10 +0500

    Fix: Reverted the changes for GTAG

--- analytics/gtag.js
+++ analytics/gtag.js
@@ -3,14 +3,14 @@ export const GA_TRACKING_ID = 'GTM-T5D3KBB';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window?.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, item_details, value }) => {
-  window.gtag('event', action, {
+  window?.gtag('event', action, {
     'event_category': category,
     'event_label': label,
     "currency": "EUR",
--- pages/_app.js
+++ pages/_app.js
@@ -1,62 +1,56 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
 import * as gtag from "../analytics/gtag";
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
-    // for google analytics
-    const router = useRouter()
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            gtag.pageview(url);
-        }
-        router.events.on('routeChangeComplete', handleRouteChange)
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange)
-        }
-    }, [router.events]);
+  // for google analytics
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events]);
 
 
-    return (
-        <>
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/*G-3V9MVCBWMK*/}
-          <Helmet>
-            <script>{`window.dataLayer = window.dataLayer || [];`}</script>
-          </Helmet>
-          <Helmet>
-            <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T5D3KBB');`}</script>
-          </Helmet>
-          <Helmet>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-T5D3KBB"></script>
-          </Helmet>
-          <Helmet>
-            <script>{`window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)};
-              gtag('js', new Date());
-              gtag('config', 'GTM-T5D3KBB');`}
-            </script>
-          </Helmet>
-            {/*Google Tag Manager (noscript)*/}
-            <noscript>
-              <iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-T5D3KBB"
-                height="0"
-                width="0"
-                style={{"display":"none","visibility":"hidden"}}
-              ></iframe>
-            </noscript>
-            {/*End Google Tag Manager (noscript)*/}
-            <Component {...pageProps} />
-        </>
-    );
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=GTM-T5D3KBB`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GTM-T5D3KBB', {
+            page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"
+          height="0"
+          width="0"
+          style={{"display":"none","visibility":"hidden"}}
+        ></iframe>
+      </noscript>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
